5c577b4168fc893366281fa0f5203a5505f0ae53
edcyc <edcyc.tw@gmail.com>
2015-11-03 19:21:56 +0800

    Build script

--- build.sh
+++ build.sh
@@ -1,25 +1,51 @@
+is_go_existing() {
+  go_version_response=$(go version)
+  if [[ $go_version_response == *"go version"* ]]; then
+    return 1
+  else
+    return 0
+  fi
+}
 
-# Install HG for one of go package to use
-sudo apt-get install -y mercurial
+install_go() {
+  # Install Go 1.5.1
+  wget https://storage.googleapis.com/golang/go1.5.1.linux-amd64.tar.gz
+  tar -C /usr/local -xzf go1.5.1.linux-amd64.tar.gz
+  
+  # Go bin path
+  export PATH=$PATH:/usr/local/go/bin
+}
 
-# Install Go 1.5.1
-wget https://storage.googleapis.com/golang/go1.5.1.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.5.1.linux-amd64.tar.gz
+set_temporarily_go_path() {
+  previous_go_path=$GOPATH
+  mkdir -p /tmp/go
+  # Export Go path
+  export GOPATH=/tmp/go
+}
 
-# Go bin path
-export PATH=$PATH:/usr/local/go/bin
+unset_temporarily_go_path() {
+  GOPATH=$previous_go_path
+  # Clean temporarily go path
+  rm -rf /tmp/go
+}
 
-mkdir -p /tmp/go
-# Export Go path
-export GOPATH=/tmp/go
+build_and_package() {
+  # Get self and dependent packagess
+  go get github.com/cloudawan/kubernetes_management_analysis
 
-# Get Kubernetes management analysis
-go get github.com/cloudawan/kubernetes_management_analysis
+  # Build
+  go build
+  mv kubernetes_management_analysis docker/kubernetes_management_analysis/
+  find ! -wholename './docker/*' ! -wholename './docker' ! -wholename '.' -exec rm -rf {} +
+  mv docker/version version
+  mv docker/environment environment
+}
 
+is_go_existing
+if [ $? == 0 ]; then
+  install_go
+fi
 
-go build
-mv kubernetes_management_analysis docker/kubernetes_management_analysis/
-find ! -wholename './docker/*' ! -wholename './docker' ! -wholename '.' -exec rm -rf {} +
-mv docker/version version
-mv docker/environment environment
-
+set_temporarily_go_path
+build_and_package
+unset_temporarily_go_path
