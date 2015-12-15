9eb345e039d6c4bf4190312a7550b0d0e42ec403
edcyc <edcyc.tw@gmail.com>
2015-12-15 18:06:01 +0800

    Change base OS and environment parameter

--- docker/run.sh
+++ docker/run.sh
@@ -40,8 +40,7 @@ fi
 
 
 # Use environment
-sed -i "s/{{KUBEAPI_HOST}}/$KUBEAPI_HOST/g" /etc/cloudone_analysis/configuration.json
-sed -i "s/{{KUBEAPI_PORT}}/$KUBEAPI_PORT/g" /etc/cloudone_analysis/configuration.json
+sed -i "s/{{KUBEAPI_CLUSTER_HOST_AND_PORT}}/$KUBEAPI_CLUSTER_HOST_AND_PORT/g" /etc/cloudone_analysis/configuration.json
 sed -i "s/{{ELASTICSEARCH_CLUSTER_HOST}}/$ELASTICSEARCH_CLUSTER_HOST/g" /etc/cloudone_analysis/configuration.json
 sed -i "s/{{ELASTICSEARCH_CLUSTER_PORT}}/$ELASTICSEARCH_CLUSTER_PORT/g" /etc/cloudone_analysis/configuration.json
 
