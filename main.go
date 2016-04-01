a037cd99fc308f9a196f7d462b74e8fdf83ed036
edcyc <edcyc.tw@gmail.com>
2016-04-02 02:27:15 +0800

    Add close for execute

--- main.go
+++ main.go
@@ -15,7 +15,7 @@
 package main
 
 import (
-	_ "github.com/cloudawan/cloudone_analysis/execute"
+	"github.com/cloudawan/cloudone_analysis/execute"
 	"github.com/cloudawan/cloudone_analysis/restapi"
 	"github.com/cloudawan/cloudone_analysis/utility/logger"
 )
@@ -24,4 +24,5 @@ var log = logger.GetLogManager().GetLogger("default")
 
 func main() {
 	restapi.StartRestAPIServer()
+	execute.Close()
 }
