02ad3538f86fe59104b591db4036d2d613dee98d
edcyc <edcyc.tw@gmail.com>
2016-04-14 20:25:47 +0800

    Initialize

--- utility/cluster/storage_elastic_search.go
+++ utility/cluster/storage_elastic_search.go
@@ -24,6 +24,7 @@ import (
 func init() {
 	createIndexTemplate()
 	// Create index
+	connection := elasticsearch.ElasticSearchClient.GetConnection()
 	connection.CreateIndex(indexClusterSingletonLock)
 }
 
